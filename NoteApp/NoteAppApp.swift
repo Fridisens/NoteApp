17eb93a3ab5188978ca7d6d188da5e94c2454cf3
Fridisens <fridisens@gmail.com>
2024-04-19 10:18:44 +0200

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Frida Dahlqvist on 2024-04-19.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
